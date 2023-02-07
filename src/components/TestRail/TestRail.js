0dfa8bac2924a5b779f87c82823c8e85f7e3e717
Surya Tripathi <raj.surya19@gmail.com>
2023-02-07 23:24:49 +0530

    Debug: adding logs for debugging error from testrails

--- src/components/TestRail/TestRail.js
+++ src/components/TestRail/TestRail.js
@@ -124,6 +124,7 @@ class TestRail {
             '/add_results_for_cases/' + runID,
             postData,
             (response) => {
+                ColorConsole.success('  response.data: ' + JSON.stringify(response.data));
                 const resultId = response.data[0].id;
 
                 ColorConsole.success('  TestRail result ' + resultId + ' sent for TestCase C' + result.getCaseId());
